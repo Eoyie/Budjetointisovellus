f02bc82291b95b52c329670df63a94685b231e25
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2023-11-26 03:13:38 +0200

    added order by date and div-element

--- expences.py
+++ expences.py
@@ -19,10 +19,14 @@ def get_all_expences(user_id):
     sql = text("SELECT name,price,category_id,date,notes FROM expences WHERE user_id=:user_id")
     result = db.session.execute(sql, {"user_id":user_id})
     expences = result.fetchall()
-    return expences
+    return order_expences_by_date(expences)
 
 def get_sum_expences(user_id):
     sql = text("SELECT SUM(price) FROM expences WHERE user_id=:user_id")
     result = db.session.execute(sql, {"user_id":user_id})
     expences_sum = result.fetchone()[0]
     return expences_sum
+
+def order_expences_by_date(expence_list):
+    expence_list.sort(key = lambda expence: expence[3], reverse=True)
+    return expence_list
