12faec28eb984a13378a3a96f6bec120e5a7b499
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2023-12-03 17:27:59 +0200

    Added budget flexbox view

--- budgets.py
+++ budgets.py
@@ -7,26 +7,45 @@ def add_budget(amount, date, notes, user_id):
     visible = True
     date = datetime.strptime(date, "%Y-%m")
     try:
-        sql = text("INSERT INTO budgets (amount,date,notes,user_id,visible) VALUES (:amount,:date,:notes,:user_id,:visible)")
-        db.session.execute(sql, {"amount":amount, "date":date, "notes":notes, "user_id":user_id, "visible":visible})
-        db.session.commit()
+        try:
+            sql = text("SELECT id FROM budgets WHERE user_id=:user_id \
+                            AND date=:date")
+            result = db.session.execute(sql, {"user_id":user_id, "date":date})
+            id = ' '.join(map(str, result.fetchone()))
+            sql = text("UPDATE budgets SET amount=:amount, notes=:notes \
+                       WHERE user_id=:user_id AND id=:id")
+            db.session.execute(sql, {"amount":amount, "notes":notes, 
+                                     "user_id":user_id, "id":id})
+            db.session.commit()
+        except:
+            sql = text("INSERT INTO budgets (amount,date,notes,user_id,visible)\
+                        VALUES (:amount,:date,:notes,:user_id,:visible)")
+            db.session.execute(sql, {"amount":amount, "date":date,
+                                     "notes":notes, "user_id":user_id,
+                                     "visible":visible})
+            db.session.commit()
     except:
         return False
     return True
 
 def this_month_budget(user_id):
-    sql = text("SELECT amount,date FROM budgets WHERE user_id=:user_id \
-               AND EXTRACT(MONTH FROM date)=:month AND EXTRACT(YEAR FROM date)=:year")
-    result = db.session.execute(sql, {"user_id":user_id})
-    budgets = result.fetchall()
-    return order_budgets_by_date(budgets)[0]
-
-def order_budgets_by_date(budget_list):
-    budget_list.sort(key = lambda budget: budget[1], reverse=True)
-    return budget_list
+    month = datetime.now().month
+    year = datetime.now().year
+    try:
+        sql = text("SELECT amount,date FROM budgets WHERE user_id=:user_id \
+                    AND EXTRACT(MONTH FROM date)=:month AND \
+                    EXTRACT(YEAR FROM date)=:year")
+        result = db.session.execute(sql, {"user_id":user_id, "month":month,
+                                          "year":year})
+        budgets = result.fetchall()
+        return budgets[0]
+    except:
+        return False
 
 def get_all_budgets(user_id):
-    sql = text("SELECT amount,date,notes FROM budgets WHERE user_id=:user_id")
+    sql = text("SELECT amount, TO_CHAR(date, 'MM-YYYY') as month, notes \
+               FROM budgets WHERE user_id=:user_id \
+               ORDER BY date DESC")
     result = db.session.execute(sql, {"user_id":user_id})
     budgets = result.fetchall()
-    return order_budgets_by_date(budgets)
+    return budgets
