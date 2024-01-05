ff4ba1f3a78e7cadee1a49ae00d6f0df500587ff
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-01-05 07:09:22 +0200

    Updated schema for future expenses

--- schema.sql
+++ schema.sql
@@ -19,6 +19,14 @@ CREATE TABLE expenses (
     user_id INTEGER REFERENCES users,
     visible BOOLEAN
     );
+CREATE TABLE future_expenses (
+    id SERIAL PRIMARY KEY,
+    price INTEGER, 
+    category_id INTEGER REFERENCES categories, 
+    notes TEXT, 
+    user_id INTEGER REFERENCES users,
+    visible BOOLEAN
+    );
 CREATE TABLE budgets (
     id SERIAL PRIMARY KEY,
     amount INTEGER,
