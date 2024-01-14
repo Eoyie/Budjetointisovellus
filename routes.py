b539b1111817d5cabfbdbddd57f7b9a97b99ea11
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2024-01-14 11:21:57 +0200

    Added a csrf token check

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, request, session, abort
 from app import app
 import users, categories, expenses, budgets, future_expenses
+import secrets
 
 @app.route("/")
 def index():
@@ -36,6 +37,7 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         if users.login(username, password):
+            session["csrf_token"] = secrets.token_hex(16)
             return redirect("/")
         return render_template("error.html",
                                 message="Väärä tunnus tai salasana")
@@ -67,6 +69,7 @@ def register():
             return render_template("error.html",
                                 message="Annettu käyttäjänimi on jo käytössä")
         if users.register(username, password1):
+            session["csrf_token"] = secrets.token_hex(16)
             return redirect("/")
         return render_template("error.html",
                                 message="Rekisteröinti ei onnistunut")
@@ -89,6 +92,8 @@ def new_expense():
         date = request.form["date"]
         category_id  = request.form["category"]
         notes = request.form["notes"]
+        if session["csrf_token"] != request.form["csrf_token"]:
+            abort(403)
         if not expenses.check_price(price):
             return render_template("error_logged_in.html",
                                 message="Menon hinta tulee olla positiivinen\
@@ -116,6 +121,8 @@ def delete_expense():
     if not session.get("logged_in"):
         return render_template("error.html",
                                message="Et ole kirjautunut sisään!")
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
     user_id = session.get("user_id")
     expense_id = request.args.get("id")
     if expenses.delete_from_view(user_id, expense_id):
@@ -133,6 +140,8 @@ def manage_categories():
         user_categories = categories.get_all_categories(user_id)
         return render_template("categories.html", categories=user_categories)
     if request.method == "POST":
+        if session["csrf_token"] != request.form["csrf_token"]:
+            abort(403)
         if request.form["action"] == "Lisaa kategoria":
             name = request.form["name"]
             if categories.add_category(name, user_id):
@@ -156,6 +165,8 @@ def manage_budgets():
         user_budgets = budgets.get_all_budgets(user_id)
         return render_template("budget.html", budgets=user_budgets)
     if request.method == "POST":
+        if session["csrf_token"] != request.form["csrf_token"]:
+            abort(403)
         budget = request.form["budget"]
         month = request.form["month"]
         if not budgets.check_budget(budget):
@@ -175,6 +186,8 @@ def delete_budget():
     if not session.get("logged_in"):
         return render_template("error.html",
                                message="Et ole kirjautunut sisään!")
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
     user_id = session.get("user_id")
     budget_id = request.args.get("id")
     if budgets.delete_from_view(user_id, budget_id):
@@ -200,6 +213,8 @@ def new_future_expense():
         price = request.form["price"]
         category_id  = request.form["category"]
         notes = request.form["notes"]
+        if session["csrf_token"] != request.form["csrf_token"]:
+            abort(403)
         if not future_expenses.check_price(price):
             return render_template("error_logged_in.html",
                                 message="Menon hinta tulee olla positiivinen\
@@ -228,6 +243,8 @@ def delete_future_expense():
     if not session.get("logged_in"):
         return render_template("error.html",
                                message="Et ole kirjautunut sisään!")
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
     user_id = session.get("user_id")
     expense_id = request.args.get("id")
     if future_expenses.delete_from_view(user_id, expense_id):
